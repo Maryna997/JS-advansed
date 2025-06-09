6ae33876c1297c7f3a912a7df54bcb30413bd755
Maryna Zuikova <mzujkova2002@gmail.com>
2025-06-09 12:22:24 +0100

    add notes 9

---
+++ topic-9/notes/TS-advantages.js
@@ -0,0 +1,39 @@
+// function add(num1, num2) {
+//     return num1 + num2;
+//   }
+  
+//   add('1', '1');
+
+
+// function add(num1: number, num2: number) {
+//     return num1 + num2;
+//   }
+  
+//   add('1', '1');
+
+
+
+// function greet(person) {
+//   return 'Hello, ' + person.firstName + ' ' + person.lastName;
+// }
+
+// let user = { firstName: 'John', lastName: 'Doe' };
+
+// console.log(greet(user)); // "Hello, John Doe"
+
+
+
+// type Person = {
+//     firstName: string;
+//     lastName: string;
+//   };
+  
+//   function greet(person: Person) {
+//     return 'Hello, ' + person.firstName + ' ' + person.lastName;
+//   }
+  
+//   let user = { firstName: 'John', lastName: 'Doe' };
+  
+//   console.log(greet(user)); // "Hello, John Doe"
+
+
